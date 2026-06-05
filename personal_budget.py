46113c7a9db3cd4b7642f34b02b1371b8e591b2f
Your Name <your.email@example.cocls>
2026-06-05 12:24:19 -0400

    in progress

--- personal_budget.py
+++ personal_budget.py
@@ -6,7 +6,15 @@ print(input("""Personal Budget Tracker
 5. View spending by category
 6. View remaining balance
 7. Exit
-Please select an option (1-7): ({input})"""))
+Please select an option (1-7): """))
 
-if input == "7":
-    print("Exiting Personal Budget Tracker. Goodbye!")
+if input == "1":
+    def add_income(budget_data, amount):
+        budget_data = {
+            "income": 0,
+            "expenses": []
+        }
+        budget_data["income"] += amount
+        print(f"Income of ${amount} added.")
+
+       
