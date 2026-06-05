12c67f23ce0cfec0b1542430d7de5298e8974902
Your Name <your.email@example.cocls>
2026-06-05 13:44:51 -0400

    in progress

--- contact_app.py
+++ contact_app.py
@@ -1,4 +1,4 @@
-print(input("""
+print(int(input("""
       Contact Book App
       1. Add a contact
       2. Search contact
@@ -6,4 +6,53 @@ print(input("""
       4. Delete contact
       5. View all contacts
       6. Exit
-      """))
+                
+      """)))
+
+input = input("Enter your choice: ")
+
+def add_contact(contacts, name, phone, email, city):
+   pass
+def search_contact(contacts, search_term):
+    pass
+def update_contact(contacts, name, field, new_value):
+    pass
+def delete_contact(contacts, name):
+    pass
+def view_all_contacts(contacts):
+    pass
+
+if input == "1":
+
+    contacts = []
+
+    name = input("Enter contact name: ")
+    phone = input("Enter contact phone: ")
+    email = input("Enter contact email: ")
+    city = input("Enter contact city: ")
+    add_contact(contacts, name, phone, email, city)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if input == "6":
+    print("Exiting the app. Goodbye!")
+
+    
