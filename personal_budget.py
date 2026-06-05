3d3dc29f310d38455a46523a9797e0446841c9c5
Your Name <your.email@example.cocls>
2026-06-05 12:14:36 -0400

    in progress

--- personal_budget.py
+++ personal_budget.py
@@ -1,3 +1,12 @@
-print("Personal Budget tracker")
-print("1.Add income")
-print("2. Add expense")
+print(input("""Personal Budget Tracker
+1. Add income
+2. Add expense
+3. View all expenses
+4. View total expenses
+5. View spending by category
+6. View remaining balance
+7. Exit
+Please select an option (1-7): ({input})"""))
+
+if input == "7":
+    print("Exiting Personal Budget Tracker. Goodbye!")
